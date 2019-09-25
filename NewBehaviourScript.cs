f8177bff8198ef585ee2db20b43fd3b8d1a7521a
Janesh Sharma <52086988+Janesh98@users.noreply.github.com>
2019-09-25 18:42:36 +0100

    add game project files

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideForce = 1000f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
